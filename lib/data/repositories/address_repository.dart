5a6a84aca4f7f2323d46395a40eaa54c661ede72
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-05-22 07:14:16 +0600

    feat: implement address repository and clean up debug logs

--- lib/data/repositories/address_repository.dart
+++ lib/data/repositories/address_repository.dart
@@ -1,12 +1,4 @@
 
-import 'package:dio/dio.dart';
-import '../../core/services/api_client.dart';
-import '../../config/constants/api_constants.dart';
-import 'package:get/get.dart' hide Response;
 
-class AddressRepository {
-  final ApiClient _apiClient = Get.find();
-
-  
-}
+class AddressRepository {}
 
