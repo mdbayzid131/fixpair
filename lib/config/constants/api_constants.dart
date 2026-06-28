d87fb7cf8d689765c3cfa071222c9c9fcbf05875
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-06-28 06:36:35 +0600

    feat: define API constants and base URLs for application endpoints

--- lib/config/constants/api_constants.dart
+++ lib/config/constants/api_constants.dart
@@ -1,7 +1,9 @@
 class ApiConstants {
   // Base URLs
-  static const String baseUrl = 'http://10.10.26.189:5000/api/v1';
-  static const String serverUrl = 'http://10.10.26.189:5000';
+  // static const String baseUrl = 'http://10.10.26.189:5000/api/v1';
+  // static const String serverUrl = 'http://10.10.26.189:5000';
+  static const String baseUrl = 'https://fixpair-live-consultancy.onrender.com/api/v1';
+  static const String serverUrl = 'https://fixpair-live-consultancy.onrender.com';
 
   static String getImageUrl(String? url) {
     const String placeholder = 'https://i.ibb.co/z5YHLV9/profile.png';
